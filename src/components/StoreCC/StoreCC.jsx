0e3ca0ae8441903bda630f87f3d9febdb5c2869f
YuriyGl86 <yuriygl86@gmail.com>
2023-10-16 11:37:08 +0300

    1

---
+++ src/components/StoreCC/StoreCC.jsx
@@ -0,0 +1,52 @@
+// import React from 'react'
+import classes from './store.module.css'
+
+// export function StoreFC(props) {
+//     const { shopItem } = props
+
+//   return (
+//     <div className={classes["main-content"]}>
+//         <h2>{ shopItem.brand }</h2>
+//         <h1>{ shopItem.title }</h1>
+//         <h3>{ shopItem.description }</h3>
+//         <div className={classes["description"]}>
+//             { shopItem.descriptionFull }
+//         </div>
+//         <div className={[classes["highlight-window"], classes["mobile"]].join(' ')}>
+//             <div className={classes["highlight-overlay"]}></div>
+//         </div>
+//         <div className={classes["divider"]}></div>
+//         <div className={classes["purchase-info"]}>
+//             <div className={classes["price"]}>{ shopItem.currency }{ shopItem.price }</div>
+//             <button>Добавить в корзину</button>
+//         </div>
+//     </div>
+//   )
+// }
+
+
+import React, { Component } from 'react'
+
+export class StoreCC extends Component {
+  render() {
+    const { shopItem } = this.props
+    return (
+        <div className={classes["main-content"]}>
+        <h2>{ shopItem.brand }</h2>
+        <h1>{ shopItem.title }</h1>
+        <h3>{ shopItem.description }</h3>
+        <div className={classes["description"]}>
+            { shopItem.descriptionFull }
+        </div>
+        <div className={[classes["highlight-window"], classes["mobile"]].join(' ')}>
+            <div className={classes["highlight-overlay"]}></div>
+        </div>
+        <div className={classes["divider"]}></div>
+        <div className={classes["purchase-info"]}>
+            <div className={classes["price"]}>{ shopItem.currency }{ shopItem.price.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 }) }</div>
+            <button>Добавить в корзину</button>
+        </div>
+    </div>
+    )
+  }
+}
